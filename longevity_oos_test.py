c48b8a31bb93df1b6ed3f1ea3ca545dd6d4e491f
Cezar Moise <cezar.moise@scylladb.com>
2025-04-23 16:52:09 +0300

    test(write-throttling): add index test

--- longevity_oos_test.py
+++ longevity_oos_test.py
@@ -12,6 +12,7 @@
 # See LICENSE for more details.
 #
 # Copyright (c) 2025 ScyllaDB
+from contextlib import ExitStack, contextmanager
 from time import sleep
 from longevity_test import LongevityTest
 from sdcm.cluster import MAX_TIME_WAIT_FOR_NEW_NODE_UP, BaseNode
@@ -19,17 +20,38 @@ from sdcm.sct_events import Severity
 from sdcm.sct_events.filters import EventsSeverityChangerFilter
 from sdcm.sct_events.loaders import CassandraStressEvent, CassandraStressLogEvent
 from sdcm.utils.adaptive_timeouts import Operations, adaptive_timeout
+from sdcm.utils.context_managers import DbNodeLogger
+from sdcm.utils.nemesis_utils.indexes import verify_query_by_index_works, wait_for_index_to_be_built
 from sdcm.utils.tablets.common import wait_no_tablets_migration_running
 
 
+@contextmanager
+def ignore_stress_errors():
+    with ExitStack() as stack:
+        stack.enter_context(EventsSeverityChangerFilter(
+            new_severity=Severity.NORMAL,
+            event_class=CassandraStressEvent,
+            extra_time_to_expiration=60
+        ))
+        stack.enter_context(EventsSeverityChangerFilter(
+            new_severity=Severity.NORMAL,
+            event_class=CassandraStressLogEvent,
+            extra_time_to_expiration=60
+        ))
+        yield
+
+
 class LongevityOutOfSpaceTest(LongevityTest):
-    def write_data(self, idx: int):
+    idx = 0  # counter for keyspace names
+
+    def write_data(self):
         round_robin = self.params.get("round_robin")
         stress_cmd = self.params.get("stress_cmd_w")
 
         if isinstance(stress_cmd, str):
             stress_cmd = [stress_cmd]
-        stress_cmd = [cmd.replace("keyspace_name", f"keyspace{idx}") for cmd in stress_cmd]
+        stress_cmd = [cmd.replace("keyspace_name", f"keyspace{self.idx}") for cmd in stress_cmd]
+        self.idx += 1
 
         params = {"stress_cmd": stress_cmd, "round_robin": round_robin}
         stress_queue = []
@@ -37,6 +59,19 @@ class LongevityOutOfSpaceTest(LongevityTest):
 
         for stress in stress_queue:
             self.verify_stress_thread(stress)
+
+    def create_secondary_index(self, column: str):
+        ks = "keyspace1"
+        cf = "standard1"
+        index_name = f"{cf}_{column}_oos"
+        node = self.db_cluster.nodes[0]
+        with DbNodeLogger(self.db_cluster.nodes, "create index", target_node=node, additional_info=f"on {ks}.{cf}.{column}"):
+            with self.db_cluster.cql_connection_patient(node, connect_timeout=300) as session:
+                session.execute(f'CREATE INDEX {index_name} ON {ks}.{cf}("{column}")', timeout=600)
+
+        with adaptive_timeout(operation=Operations.CREATE_INDEX, node=node, timeout=14400) as timeout:
+            wait_for_index_to_be_built(node, ks, index_name, timeout=timeout * 2)
+        verify_query_by_index_works(session, ks, cf, column)
 
     def start_background_read(self):
         round_robin = self.params.get("round_robin")
@@ -59,14 +94,13 @@ class LongevityOutOfSpaceTest(LongevityTest):
         self.db_cluster.wait_for_nodes_up_and_normal(nodes=added_nodes)
         return added_nodes
 
-    def test_oos_scale_out(self):
+    def test_oos_write_scale_out(self):
         self.run_prepare_write_cmd()
         self.start_background_read()
 
         sleep(600)
-        with EventsSeverityChangerFilter(new_severity=Severity.NORMAL, event_class=CassandraStressEvent, extra_time_to_expiration=60), \
-                EventsSeverityChangerFilter(new_severity=Severity.NORMAL, event_class=CassandraStressLogEvent, extra_time_to_expiration=60):
-            self.write_data(1)
+        with ignore_stress_errors():
+            self.write_data()
 
         sleep(600)
         self.scale_out()
@@ -74,4 +108,36 @@ class LongevityOutOfSpaceTest(LongevityTest):
             wait_no_tablets_migration_running(node)
 
         sleep(600)
-        self.write_data(2)
+        self.write_data()
+
+    def test_oos_si_scale_out(self):
+        self.run_prepare_write_cmd()
+        # self.start_background_read()
+
+        sleep(600)
+        # create index
+        ks = "keyspace1"
+        cf = "standard1"
+        column = "C0"
+        index_name = f"{cf}_{column}_oos"
+        node = self.db_cluster.nodes[0]
+        with DbNodeLogger(self.db_cluster.nodes, "create index", target_node=node, additional_info=f"on {ks}.{cf}.{column}"):
+            with self.db_cluster.cql_connection_patient(node, connect_timeout=300) as session:
+                session.execute(f'CREATE INDEX {index_name} ON {ks}.{cf}("{column}")', timeout=600)
+
+        try:
+            with adaptive_timeout(operation=Operations.CREATE_INDEX, node=node, timeout=3600) as timeout:
+                wait_for_index_to_be_built(node, ks, index_name, timeout=timeout * 2)
+        except TimeoutError:
+            # index creation will not finish due to lack of space
+            pass
+
+        sleep(600)
+        self.scale_out()
+        for node in self.db_cluster.nodes:
+            wait_no_tablets_migration_running(node)
+
+        sleep(600)
+        with adaptive_timeout(operation=Operations.CREATE_INDEX, node=node, timeout=3600) as timeout:
+            wait_for_index_to_be_built(node, ks, index_name, timeout=timeout * 2)
+        verify_query_by_index_works(session, ks, cf, column)
